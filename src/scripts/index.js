c7349174a3cf833f9fb0165ec79ef8df85bb7e96
chuboot <chuboot.id@gmail.com>
2024-03-27 22:44:59 +0800

    edit card and nav color

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,44 +7,43 @@ console.log('Hello Coders! :)');
 let btn = document.querySelector(".burger-btn");
 let menu = document.querySelector(".nav-links");
 let links = document.querySelectorAll(".nav-links li a");
-btn.addEventListener("click", function () {
+btn.addEventListener("click", () => {
   menu.classList.toggle("nav-active");
 });
 
 for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function () {
+  links[i].addEventListener("click", () => {
     menu.classList.toggle("nav-active");
   });
 }
 
-//change bg when scroll
-// let navbar = document.querySelector("header");
+// change bg when scroll
+let navbar = document.querySelector("nav");
 
-// window.addEventListener("scroll", function () {
-//   let valueScroll = window.scrollY;
-//   console.log(valueScroll);
-//   if (valueScroll < 70) {
-//     navbar.classList.remove("bg-nav-color");
-//   } else {
-//     navbar.classList.add("bg-nav-color");
-//   }
-// });
+window.addEventListener("scroll", function () {
+  let valueScroll = window.scrollY;
+  console.log(valueScroll);
+  if (valueScroll < 70) {
+    navbar.classList.remove("bg-nav-color");
+  } else {
+    navbar.classList.add("bg-nav-color");
+  }
+});
 
 const getCards = async () => {
     const response = await fetch('../data/DATA.json');
 
     const responseJson = await response.json();
     const resto = responseJson.restaurants;
-    // console.log(resto);
     renderAllResto(resto);
 }
 
 const renderAllResto = (resto) =>{
     const listRestoElement = document.querySelector('.wrapper')
     listRestoElement.innerHTML = '';
-    resto.forEach((res) => {
+    resto.forEach((res, i) => {
         listRestoElement.innerHTML += `
-         <div class="card">
+         <div class="card" tabindex="${i}">
               <img src="${res.pictureId}" alt="">
               <span>${res.city}</span>
               <div class="card-content">
